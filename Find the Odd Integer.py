555e52c5c2adf13c7684ac327e44494ce25c25bd
Jannatun Nur Niha <jannatunnurniha@gmail.com>
2025-07-23 02:01:38 +0600

    Update Find the Odd Integer.py

--- Find the Odd Integer.py
+++ Find the Odd Integer.py
@@ -1,8 +1,20 @@
+# Function to fina an integer that appears odd numbers of times
 def find_odd_integer(arr):
+
+    # Loop through every single number in the array
     for num in set(arr):
+
+        # Check if the count of the number is odd
         if arr.count(num) % 2 != 0:
+
+            # Return the number if it appears odd numbers of times
             return num
 
+# Read input from the user and convert it to list of integers
 numbers = list(map(int, input().split()))
+
+# Call the function with the input list
 result = find_odd_integer(numbers)
+
+# Print the output
 print(result)
